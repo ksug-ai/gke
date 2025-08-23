1b0658fc1017569dd3068c6d5f14a2f042aa3f04
Yongkang He <he@yongkang.cloud>
2025-08-23 17:10:20 +1000

    Update velero to 1.16.2

--- velero-deploy.sh
+++ velero-deploy.sh
@@ -4,11 +4,11 @@ starttime=$(date +%s)
 
 echo "-------Download and Install verlero CLI if needed"
 if [ ! -f ~/gke-casa/velero ]; then
-  wget https://github.com/vmware-tanzu/velero/releases/download/v1.12.0/velero-v1.12.0-linux-amd64.tar.gz
-  tar -zxvf velero-v1.12.0-linux-amd64.tar.gz
-  sudo mv velero-v1.12.0-linux-amd64/velero ~/gke-casa
-  sudo rm velero-v1.12.0-linux-amd64.tar.gz
-  sudo rm -rf velero-v1.12.0-linux-amd64
+  wget https://github.com/vmware-tanzu/velero/releases/download/v1.16.2/velero-v1.16.2-linux-amd64.tar.gz
+  tar -zxvf velero-v1.16.2-linux-amd64.tar.gz
+  sudo mv velero-v1.16.2-linux-amd64/velero ~/gke-casa
+  sudo rm velero-v1.16.2-linux-amd64.tar.gz
+  sudo rm -rf velero-v1.16.2-linux-amd64
 fi
 
 echo "-------Create a GCS storage bucket if not exist"
@@ -68,7 +68,7 @@ fi
 echo "-------Install velero using the SA"
 velero install \
     --provider gcp \
-    --plugins velero/velero-plugin-for-gcp:v1.6.0 \
+    --plugins velero/velero-plugin-for-gcp:v1.12.2 \
     --bucket $(cat bucket4velero1) \
     --use-node-agent \
     --uploader-type restic \
