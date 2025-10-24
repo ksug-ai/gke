33583bee9223a11ce91ded7a35346eeb360f6d93
Yongkang He <he@yongkang.cloud>
2025-10-24 13:02:53 +1100

    Update PostgreSQL deployment script to include helm repo update and improve installation command formatting

--- postgresql-deploy.sh
+++ postgresql-deploy.sh
@@ -2,5 +2,8 @@ echo '-------Deploy a PostgreSQL sample database'
 
 kubectl create namespace yong-postgresql
 helm repo add bitnami https://charts.bitnami.com/bitnami
-helm install --namespace yong-postgresql postgres bitnami/postgresql --set primary.persistence.size=1Gi
+helm repo update
+helm install --namespace yong-postgresql postgres bitnami/postgresql \
+  --set image.tag=16.3.0-debian-12-r20 \
+  --set primary.persistence.size=1Gi
 kubectl -n yong-postgresql annotate pod/postgres-postgresql-0 backup.velero.io/backup-volumes=data
