da500f5fe036803e9098529045eab2c7f2f2a429
Yongkang He <he@yongkang.cloud>
2025-10-24 18:05:30 +1000

    Refactor Helm repository update logic in PostgreSQL deployment script for improved error handling

--- postgresql-deploy.sh
+++ postgresql-deploy.sh
@@ -1,8 +1,10 @@
 echo '-------Deploy a PostgreSQL sample database'
 
 kubectl create namespace yong-postgresql
-helm repo add bitnami https://charts.bitnami.com/bitnami
-helm repo update
+helm repo add bitnami https://charts.bitnami.com/bitnami 2>/dev/null || true
+if [ ! -f ~/.cache/helm/repository/bitnami-index.yaml ] || [ $(find ~/.cache/helm/repository/bitnami-index.yaml -mtime +7 2>/dev/null | wc -l) -gt 0 ]; then
+  helm repo update bitnami
+fi
 helm install postgres bitnami/postgresql \
   --namespace yong-postgresql \
   --create-namespace \
