703dbd250e72e4585fa37c2ba816819ca8996d4b
Yongkang He <he@yongkang.cloud>
2025-10-24 13:06:25 +1100

    Enhance PostgreSQL deployment and destruction scripts with improved installation command and wait option for uninstallation

--- postgresql-deploy.sh
+++ postgresql-deploy.sh
@@ -3,7 +3,11 @@ echo '-------Deploy a PostgreSQL sample database'
 kubectl create namespace yong-postgresql
 helm repo add bitnami https://charts.bitnami.com/bitnami
 helm repo update
-helm install --namespace yong-postgresql postgres bitnami/postgresql \
-  --set image.tag=16.3.0-debian-12-r20 \
-  --set primary.persistence.size=1Gi
+helm install postgres bitnami/postgresql \
+  --namespace yong-postgresql \
+  --create-namespace \
+  --set image.tag=16.3.0-debian-12-r22 \
+  --set primary.persistence.size=1Gi \
+  --set primary.persistence.labels.created-by=gke-deploy-script \
+  --wait --timeout 5m0s
 kubectl -n yong-postgresql annotate pod/postgres-postgresql-0 backup.velero.io/backup-volumes=data
--- postgresql-destroy.sh
+++ postgresql-destroy.sh
@@ -1,4 +1,5 @@
 echo '-------Destroy a PostgreSQL sample database'
 
 helm uninstall postgres -n yong-postgresql
+helm uninstall postgres -n yong-postgresql --wait
 kubectl delete namespace yong-postgresql
